1110eab3d198938b060b42b20bed57f7a772c805
Lucas Hardy <1124114+TheNicestGuy@users.noreply.github.com>
2019-08-04 03:32:20 -0400

    Add the basics of OuterDoors and InnerDoors properties to Airlock

--- Airlock.cs
+++ Airlock.cs
@@ -33,10 +33,104 @@ namespace IngameScript
             /// <remarks>
             /// This is both functional, as it appears in the configuration data
             /// to associate separate blocks into a unified airlock, and
-            /// user-friendly, as it may appear in-game on informational
+            /// cosmetic, as it may appear in-game on informational
             /// readouts.
             /// </remarks>
             public string Name { get; set; }
+
+            #region Mandatory Blocks
+
+            #region OuterDoors
+
+            private IList<IMyDoor> _outerDoors;
+            /// <summary>
+            /// Returns references to all of this airlock's "outer doors":
+            /// passable doors that can seal the airlock chamber from the
+            /// vacuum.
+            /// </summary>
+            /// <remarks>
+            /// <para>Eligible block types are the Airtight Hangar Door, the
+            /// "Door" (the halves pull straight out into the walls), and the
+            /// Sliding Door (halves rotate against the walls). Blast Doors are
+            /// not airtight in any combination, thus not eligible. Exotic
+            /// constructs that somehow manage to create a seal with
+            /// piston/rotor subgrids are not recognized by this script.</para>
+            /// <para>The returned collection is immutable, though the doors
+            /// themselves can be manipulated by reference.</para>
+            /// </remarks>
+            public IEnumerable<IMyDoor> OuterDoors
+            {
+                get
+                {
+                    return new List<IMyDoor>(_outerDoors);
+                }
+            }
+
+            public void AddOuterDoor(IMyDoor newDoor)
+            {
+                this._outerDoors.Add(newDoor);
+            }
+
+            //TODO Add methods to manipulate outer doors, like opening, closing, locking
+
+            #endregion // OuterDoors
+
+            #region InnerDoors
+
+            private IList<IMyDoor> _innerDoors;
+            /// <summary>
+            /// Returns references to all of this airlock's "inner doors":
+            /// passable doors that can seal the airlock chamber from the
+            /// habitat.
+            /// </summary>
+            /// <remarks>
+            /// <para>Eligible block types are the Airtight Hangar Door, the
+            /// "Door" (the halves pull straight out into the walls), and the
+            /// Sliding Door (halves rotate against the walls). Blast Doors are
+            /// not airtight in any combination, thus not eligible. Exotic
+            /// constructs that somehow manage to create a seal with
+            /// piston/rotor subgrids are not recognized by this script.</para>
+            /// <para>The returned collection is immutable, though the doors
+            /// themselves can be manipulated by reference.</para>
+            /// </remarks>
+            public IEnumerable<IMyDoor> InnerDoors
+            {
+                get
+                {
+                    return new List<IMyDoor>(_innerDoors);
+                }
+            }
+
+            public void AddInnerDoor(IMyDoor newDoor)
+            {
+                this._innerDoors.Add(newDoor);
+            }
+
+            //TODO Add methods to manipulate inner doors, like opening, closing, locking
+
+            #endregion // InnerDoors
+
+            #endregion // Mandatory Blocks
+
+            #region Constructors
+
+            private Airlock()
+            {
+                this._outerDoors = new List<IMyDoor>();
+                this._innerDoors = new List<IMyDoor>();
+            }
+
+            /// <summary>
+            /// Create a new instance of the Airlock class with a given name.
+            /// </summary>
+            /// <param name="name"></param>
+            public Airlock(string name) : this()
+            {
+                this.Name = name;
+
+            }
+
+            #endregion // Constructors
         }
     }
 }
